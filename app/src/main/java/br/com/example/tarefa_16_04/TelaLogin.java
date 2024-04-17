6dd7ba18a1e6eabd5b18e7f7cd4bcc198aa9e6fa
Hell0Bat <brucetdknbat@gmail.com>
2024-04-16 22:41:25 -0400

    Tarefa completa

---
+++ app/src/main/java/br/com/example/tarefa_16_04/TelaLogin.java
@@ -0,0 +1,15 @@
+package br.com.example.tarefa_16_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); // Certifique-se de criar o layout activity_tela_login.xml
+
+        // Aqui você pode adicionar o código necessário para a tela de login, como campos de entrada, botões, etc.
+    }
+}
